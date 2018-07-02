520efa2d0d3c87b53ac80776b9f397df1d9f8120
Alan Chen <alanchen1218@gmail.com>
2018-07-02 15:10:34 -0700

    video player, with youtube api, lodash, react, components, etc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -18,9 +19,12 @@ class App extends Component {
     this.state = {
       videos: [],
       selectedVideo: null
-    }
+    };
+    this.videoSearch('fortnite');
+  }
 
-    YTSearch({key: API_KEY, term: 'fortnite'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
        this.setState({
          videos: videos,
          selectedVideo: videos[0]
@@ -31,9 +35,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) =>  { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
